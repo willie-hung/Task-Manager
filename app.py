5a6cac48509c824e7877183a169b170f0e061bb1
Willie-The-Lord <willie880201044@gmail.com>
2022-12-14 00:38:41 -0600

    update project

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+# 用此檔案名稱創一個Flask的instance
 app = Flask(__name__)
+# 將app連接到我們database: sqlite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
+# Initialize我們的database ORM
 with app.app_context():
     db = SQLAlchemy(app)
 
@@ -16,28 +19,28 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-
 @app.route('/', methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
 
-        # push to database
+        # 把欲新增的task push to database
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-
+        
+        # 如果fail的話，跳error
         except:
             return 'There was as issue adding your task'
 
     else:
-        # display all current tasks
+        # 顯示我們新增的task到主頁
         tasks = Todo.query.order_by(Todo.date_created).all()
+        # 重新render我們的主頁
         return render_template('index.html', tasks=tasks)
 
-# delete
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -46,25 +49,29 @@ def delete(id):
         db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
+    # 如果fail的話，跳error
     except:
         return 'There was a problem deleting that task'
 
-# update
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task_to_update = Todo.query.get_or_404(id)
     if request.method == 'POST':
         task_to_update.content = request.form['content']
         
-        # update the database
+        # 把欲更新的task update to database
         try:
             db.session.commit()
             return redirect('/')
+
+        # 如果fail的話，跳error
         except:
             return 'There was an issue updating your task'
         
     else:
+        # 重新render我們的主頁
         return render_template('update.html', task=task_to_update)
 
+# flask app 的 driver code
 if __name__ == '__main__':
     app.run(debug=True)
